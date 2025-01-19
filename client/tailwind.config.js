4f83f0670d39b53ecee723bd5c3ad0982f3a960e
Arman Chinai <chinaiarman@gmail.com>
2025-01-18 19:51:52 -0800

    Add color styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,18 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", // Covers React components
+    "./public/index.html",        // Covers HTML files
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        platinum: "#E5E4E2",
+        "tango-pink": "#E4717A",
+        "orange-yellow": "#F5C71A",
+        toolbox: "#746CC0",
+        "bright-lavender": "#BF94E4",
+      },
+    },
   },
   plugins: [],
-}
+};
