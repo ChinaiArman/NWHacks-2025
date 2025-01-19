cd7c7205442aefd50c1957fc268f3614d707a7c2
Arman Chinai <chinaiarman@gmail.com>
2025-01-18 16:46:20 -0800

    idk why i imported this everywhere

--- server/api/classroom_routes.py
+++ server/api/classroom_routes.py
@@ -4,7 +4,7 @@
 # IMPORTS
 from flask import Blueprint, jsonify, request, current_app, session
 
-from services.decorators import verified_login_required, unverified_login_required
+from services.decorators import verified_login_required
 
 
 # DEFINE BLUEPRINT
